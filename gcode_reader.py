472701b7340c4500ad977f94e3953e03312e33ed
Nuno Frutuoso <nunoammfrutuoso@gmail.com>
2020-10-13 16:17:36 +0100

    Adding files to project

---
+++ gcode_reader.py
@@ -0,0 +1,420 @@
+start_gcode_string = ";LAYER:"
+setting_string = ";SETTING_3 "
+end_gcode_string = ";TIME_ELAPSED:"
+skin_string = ";TYPE:SKIN\n"
+outer_wall_string = ";TYPE:WALL-OUTER"
+ironing_enable_string = "ironing_enabled"
+mesh_string = ";MESH:"
+fan_string = "M106 S"
+ironing_string = ";TYPE:SKIN ;IRONING\n"
+
+
+class Part:
+    def __init__(self, filename):
+
+        self.filename = filename
+        self.root = filename
+        self.file_lines = []
+
+        directories = filename.split('/')
+        self.filename = directories[-1]
+
+        self.ironing_file = ""
+        for dir in directories[:-1]:
+            self.ironing_file += dir + "/"
+
+        self.ironing_file += "ironing_" + self.filename
+        self.ironing_moves = None
+        self.ironing_rotated_moves = None
+
+        self.ironing_flows = []
+        self.list_of_layers = []
+
+        self.start_gcode = []
+        self.end_gcode = []
+        # self.layers_lines = []
+
+        self.ironing_enabled = 0
+        self.ironing_flow = 0
+        self.ironing_inset = 0
+        self.speed_ironing = 0
+        self.settings = {}
+        self.part_has_ironing = False
+        self.ironing_settings = {}
+        #self.has_ironing = self.search_for_layers()
+        self.layer_lines = []
+
+        self.fan_speed = 0
+        self.zoffset = 0
+
+    def read_file(self):
+        with open(self.root, 'r') as file:
+            self.file_lines = file.readlines()
+
+    def get_start_gcode_from_file(self):
+        for line in self.file_lines:
+            if start_gcode_string not in line:
+                self.start_gcode.append(line)
+            else:
+                break
+
+    def get_end_gcode_from_file(self):
+        end_gcode_string = ";TIME_ELAPSED:"
+        for line in self.file_lines[-1:0:-1]:
+            if end_gcode_string not in line:
+                self.end_gcode.append(line)
+            else:
+                self.end_gcode.reverse()
+                break
+
+    def get_settings_from_file(self):
+        settings_line = ""
+        for line in self.file_lines:
+            if setting_string in line:
+                settings_line += line.replace(setting_string, "").strip("\n")
+
+        settings = settings_line.split("\\n")
+
+        for setting in settings:
+            if " = " in setting:
+                setting_name, setting_value = setting.strip("\\").split(" = ")
+                self.settings[setting_name] = setting_value
+
+    def check_for_ironing(self):
+        if ironing_enable_string in self.settings:
+            self.part_has_ironing = self.settings[ironing_enable_string] == "True"
+            self.ironing_settings = [(key, value) for key, value in self.settings.items() if "ironing" in key]
+        else:
+            self.part_has_ironing = False
+
+        return self.part_has_ironing
+
+    def get_part_instructions(self):
+
+        self.get_start_gcode_from_file()
+        self.get_end_gcode_from_file()
+
+        last_extrusion = 0
+
+        if self.check_for_ironing():
+            start_lines = [(line, index) for index, line in enumerate(self.file_lines) if start_gcode_string in line]
+            end_lines = [(line, index) for index, line in enumerate(self.file_lines) if end_gcode_string in line]
+
+            if len(start_lines) == len(end_lines):
+                for i in range(len(start_lines)):
+                    self.layer_lines.append((start_lines[i][0], start_lines[i][1], end_lines[i][1]))
+                    new_layer = Layer(i, self.file_lines[start_lines[i][1]:end_lines[i][1] + 1])
+
+                    if i == 0:
+                        last_extrusion = 0
+                    elif i == len(start_lines) - 1:
+                        ironing_instructions = new_layer.get_ironing_instructions_from_layer()
+                        self.ironing_moves = Layer(-1, ironing_instructions)
+                        self.ironing_moves.set_ironing_skin_code()
+
+                        rotated_moves = self.ironing_moves.rotate_ironing_instructions()
+                        self.ironing_rotated_moves = Layer(-1, rotated_moves)
+                        self.ironing_rotated_moves.set_ironing_skin_code()
+
+                    last_extrusion = new_layer.get_extrusion_length(last_extrusion)
+
+                    self.list_of_layers.append(new_layer)
+
+                self.ironing_moves.get_extrusion_length(last_extrusion)
+                self.ironing_rotated_moves.get_extrusion_length(last_extrusion)
+
+                for layer in self.list_of_layers:
+                    print("Start: %f  End: %f  Length:%f" % (layer.extrusion_start, layer.extrusion_end, layer.extrusion_length))
+
+                layer = self.ironing_moves
+                print("Ironing\nStart: %f  End: %f  Length:%f" % (layer.extrusion_start, layer.extrusion_end, layer.extrusion_length))
+            else:
+                print("List size mismatch")
+
+        else:
+            print("Part has no Iroring")
+
+        self.get_z_height_for_part()
+        self.get_fan_speed_for_part()
+
+    def add_ironing_to_part(self, layers_to_iron, direction, disable_flow=None, fan_speed=None, z_offset=None):
+        with open(self.ironing_file, 'w') as file:
+            file.writelines(self.start_gcode)
+
+            length_increment = 0
+            for index, iron in enumerate(layers_to_iron):
+                if iron == 1:
+                    if direction == "opta":
+                        if (len(self.list_of_layers) - index) % 2 == 0:
+                            layer_gcode = self.list_of_layers[index].get_gcode_modified(length_increment, self.ironing_moves, disable_flow, fan_speed, z_offset)
+                        else:
+                            layer_gcode = self.list_of_layers[index].get_gcode_modified(length_increment, self.ironing_rotated_moves, disable_flow, fan_speed, z_offset)
+                        length_increment += self.ironing_rotated_moves.extrusion_length
+                    elif direction == "optb":
+                        if (len(self.list_of_layers) - index) % 2 == 0:
+                            layer_gcode = self.list_of_layers[index].get_gcode_modified(length_increment, self.ironing_rotated_moves, disable_flow, fan_speed, z_offset)
+                        else:
+                            layer_gcode = self.list_of_layers[index].get_gcode_modified(length_increment, self.ironing_moves, disable_flow, fan_speed, z_offset)
+                        length_increment += self.ironing_moves.extrusion_length
+                else:
+                    layer_gcode = self.list_of_layers[index].get_gcode_modified(length_increment)
+
+                file.writelines(layer_gcode)
+
+            new_line = get_new_extrusion_on_gcode_line(self.end_gcode[0], length_increment)
+            if new_line:
+                file.write(new_line)
+                file.writelines(self.end_gcode[1:])
+            else:
+                file.writelines(self.end_gcode[1:])
+            # file.writelines(self.settings)
+
+        return True
+
+    def set_advanced_settings(self, fan_speed, zoffset):
+        self.fan_speed = fan_speed
+        self.zoffset = zoffset
+
+        for layer in self.list_of_layers:
+            layer.get_initial_fan_speed()
+
+    def get_z_height_for_part(self):
+        for layer in self.list_of_layers:
+            h = layer.get_z_height()
+            # print("L%s : %s" % (layer.layer_index, h))
+
+    def get_fan_speed_for_part(self):
+        fan_lines = [line for line in self.file_lines if fan_string in line]
+        m_code, speed = fan_lines[0].strip("\n").split("S")
+        last_fan_speed = float(speed)
+        for layer in self.list_of_layers:
+            last_fan_speed = layer.get_fan_speed(last_fan_speed)
+            # print("L%s : %s" % (layer.layer_index, last_fan_speed))
+
+
+class Layer:
+
+    def __init__(self, index, gcode_lines):
+        self.layer_index = index
+        self.gcode_lines = gcode_lines
+        self.extrusion_length = 0
+        self.extrusion_start = 0
+        self.extrusion_end = 0
+        self.extrusion_to_add = 0
+
+        self.z_height = 0
+        self.fan_speed = 0
+
+        self.ironing_z_height = 0
+        self.ironing_fan_speed = 0
+        self.ironing_flow = 0
+
+        self.number_of_parts = 1
+        self.get_number_of_parts()
+
+    def set_ironing_skin_code(self):
+        self.gcode_lines[0] = ironing_string
+
+    def get_extrusion_length(self, starting_extrusion):
+        self.extrusion_start = starting_extrusion
+        self.extrusion_end = 0
+        extrusion_reg = []
+
+        for line in self.gcode_lines:
+            if "G1" in line and "E" in line:
+                substring = line.strip("\n").split(" ")
+                substring.sort()
+                extrusion_reg.append(float(substring[0].replace("E", "")))
+
+        self.extrusion_end = max(extrusion_reg)
+        self.extrusion_length = self.extrusion_end - self.extrusion_start
+
+        return self.extrusion_end
+
+    def get_number_of_parts(self):
+        self.number_of_parts = self.gcode_lines.count(mesh_string)
+
+        if self.number_of_parts == 0:  # old versions
+            self.number_of_parts = 1
+
+    def get_fan_speed(self, fan_speed):
+        fan_lines = [line for line in self.gcode_lines if fan_string in line]
+        if fan_lines:
+            aux = fan_lines[-1]
+            m_code, s_code = aux.strip("\n").split("S")
+            self.fan_speed = float(s_code)
+        else:
+            self.fan_speed = fan_speed
+
+        return self.fan_speed
+
+    def get_z_height(self):
+        z_heights = [line for line in self.gcode_lines if ("G1" in line or "G0" in line) and "Z" in line]
+        if z_heights:
+            aux = z_heights[0]
+            g_code, z_code = aux.strip("\n").split("Z")
+            self.z_height = float(z_code)
+        else:
+            self.z_height = -1
+
+        return self.z_height
+
+
+    def get_ironing_instructions_from_layer(self):
+        if self.number_of_parts == 1:
+            skin_in_gcode = [index for index, line in enumerate(self.gcode_lines) if skin_string in line]
+            ironing_in_gcode = self.gcode_lines[skin_in_gcode[-1]:-1]
+            self.gcode_lines = self.gcode_lines[:skin_in_gcode[-1]] + [self.gcode_lines[-1]]
+            return ironing_in_gcode
+        else:
+            pass
+
+    def rotate_ironing_instructions(self):
+        return rotate_gcode_lines(self.gcode_lines)
+
+    def get_gcode_modified(self, extrusion_length, ironing=None, disable_flow=False, fan_speed=-1, z_offset=0):
+        gcode = []
+        for line in self.gcode_lines[:-1]:
+            if "G1" in line and "E" in line:
+                new_line = get_new_extrusion_on_gcode_line(line, extrusion_length)
+                if new_line:
+                    gcode.append(new_line)
+            else:
+                gcode.append(line)
+
+        ironing_gcode = []
+        if ironing:
+            for line in ironing.gcode_lines:
+                if "G1" in line and "E" in line:
+                    if disable_flow:
+                        new_line = get_gcode_line_without_extrusion(line)
+                        if new_line != "":
+                            ironing_gcode.append(new_line)
+                    else:
+                        new_line = get_new_extrusion_on_gcode_line(line, self.extrusion_end + extrusion_length - ironing.extrusion_start)
+                        if new_line:
+                            ironing_gcode.append(new_line)
+                else:
+                    ironing_gcode.append(line)
+
+            if fan_speed >= 0:
+                ironing_gcode.insert(1, "M106 S%s\n" % fan_speed)
+                ironing_gcode.insert(len(ironing_gcode), "M106 S%s\n" % self.fan_speed)
+
+            if z_offset != 0:
+                ironing_gcode.insert(1, "G0 Z%s\n" % str(self.z_height + z_offset))
+                ironing_gcode.insert(len(ironing_gcode), "G0 Z%s\n" % str(self.z_height))
+
+            return gcode + ironing_gcode + list(self.gcode_lines[-1])
+
+        else:
+            return gcode + list(self.gcode_lines[-1])
+
+
+def get_new_extrusion_on_gcode_line(line, extrusion_length):
+    aux_line = line.strip("\n").split("E")
+    if len(aux_line) == 2:
+        if len(aux_line[1].split(" ")) == 1:
+            return "%sE%.5f\n" % (aux_line[0], float(aux_line[1]) + extrusion_length)
+    else:
+        print("Error while incrementing the extrusion length.")
+        return None
+
+def get_gcode_line_without_extrusion(line):
+    aux_line = line.strip("\n").split(" ")
+    new_gcode = ""
+    if "E" in line and "F" in line and len(aux_line) <= 3:
+        pass
+    else:
+        for command in aux_line:
+            if "E" not in command:
+                new_gcode += "%s " % command
+        new_gcode += "\n"
+
+    return new_gcode
+
+def rotate_gcode_lines(list_of_gcode):
+    ironing_rotated_moves = []
+    center_of_mass = get_center_of_mass_of_gcode(list_of_gcode)
+
+    for line in list_of_gcode:
+        if "G1" in line or "G0" in line:
+            point_x = None
+            point_y = None
+
+            strip_x = line.split('X')
+            if len(strip_x) > 1:
+                strip_x_2 = strip_x[-1].split(" ")
+                point_x = float(strip_x_2[0])
+
+            strip_y = line.split('Y')
+            if len(strip_y) > 1:
+                strip_y_2 = strip_y[-1].split(" ")
+                point_y = float(strip_y_2[0])
+
+            if point_x and point_y:
+                point_x_orig = point_x - center_of_mass[0]
+                point_y_orig = point_y - center_of_mass[1]
+
+                new_point_x = - point_x_orig + center_of_mass[0]
+                new_point_y = point_y_orig + center_of_mass[1]
+
+                split_line = line.strip("\n").split(" ")
+                if len(split_line) == 3:
+                    # aux_line = "%s X%f Y%f\n" % (split_line[0], point.x, point_y)
+                    aux_line = "%s X%f Y%f\n" % (split_line[0], new_point_x, new_point_y)
+                    ironing_rotated_moves.append(aux_line)
+                elif len(split_line) == 4:
+                    # aux_line = "%s X%f Y%f %s" % (split_line[0], point.x, point_y, split_line[-1])
+                    if "F" in line and "E" in line:
+                        aux_line = "%s %s X%f Y%f %s\n" % (split_line[0], split_line[1], new_point_x, new_point_y, split_line[-1])
+                    elif "E" in line:
+                        aux_line = "%s X%f Y%f %s\n" % (split_line[0], new_point_x, new_point_y, split_line[-1])
+                    elif "F" in line:
+                        aux_line = "%s %s X%f Y%f\n" % (split_line[0], split_line[1], new_point_x, new_point_y)
+
+                    ironing_rotated_moves.append(aux_line)
+                elif len(split_line) == 5:
+                    print("This happens!!!")
+                else:
+                    print("Something wrong")
+
+            else:
+                ironing_rotated_moves.append(line)
+        else:
+            ironing_rotated_moves.append(line)
+
+    return ironing_rotated_moves
+
+def get_center_of_mass_of_gcode(list_of_gcode):
+    list_of_points = []
+
+    for line in list_of_gcode:
+        if "G1" in line or "G0" in line:
+            point_x = None
+            point_y = None
+
+            strip_x = line.split('X')
+            if len(strip_x) > 1:
+                strip_x_2 = strip_x[-1].split(" ")
+                point_x = float(strip_x_2[0])
+
+            strip_y = line.split('Y')
+            if len(strip_y) > 1:
+                strip_y_2 = strip_y[-1].split(" ")
+                point_y = float(strip_y_2[0])
+
+            if point_x and point_y:
+                # point = Point([point_x, point_y])
+                point = [point_x, point_y]
+                list_of_points.append(point)
+
+    if list_of_points:
+        points_x = [x[0] for x in list_of_points]
+        points_y = [x[1] for x in list_of_points]
+        center_of_mass = [sum(points_x) / len(points_x), sum(points_y) / len(points_y)]
+    else:
+        center_of_mass = [0, 0]
+        print("No points found...")
+
+    return center_of_mass
---
+++ main.py
@@ -0,0 +1,208 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog  # "CSS" for ttk
+
+from gcode_reader import Part
+
+
+def add_new_file(show=False):
+    root = filedialog.askopenfilename(title="Select file", filetypes=(("Gcode files", "*.gcode"), ("all files", "*.*")))
+    if show:
+        print("Open file: %s " % root)
+    return root
+
+
+def save_new_file(default_name="", show=False):
+    root = filedialog.asksaveasfilename(title="Select file", defaultextension=".gcode", initialfile=default_name, filetypes=(("Gcode", "*.gcode"), ("all files", "*.*")))
+    if show:
+        print("Save file: %s " % root)
+    return root
+
+
+def quit_confirmation(root):
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.quit()
+
+
+def popup_message(message):
+    if messagebox.showerror("Error", message):
+        pass
+
+
+def success_popup_message(message):
+    if messagebox.showinfo("Success", message):
+        pass
+
+class GUI(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+        tk.Tk.wm_title(self, "Ironing Experiments")
+
+        self.main_container = tk.Frame(self)
+        self.main_container.grid(row=0, column=0, padx=10, pady=5, sticky="n")
+
+        file_settings = ttk.LabelFrame(self.main_container, text="File")
+        file_settings.grid(row=0, column=0, padx=5, pady=5, sticky='nwes')
+        open_file = ttk.Button(file_settings, text="Open", width=15, command=self.start_ironing_object)
+        open_file.grid(row=0, column=0)
+
+        # Defining variables:
+        self.ironing_settings = None
+
+        self.part = None
+
+        self.radio_var = None
+        self.start_layer = None
+        self.end_layer = None
+        self.interval = None
+        self.list_ironings = None
+        self.ironing = None
+
+    def after_file_open(self):
+        # OPTIONS READ FROM FILE
+        self.read_settings = ttk.LabelFrame(self.main_container, text="Read from file")
+        self.read_settings.grid(row=1, column=0, padx=5, pady=5, sticky='nwes')
+
+        tk.Label(self.read_settings, text="File Name:").grid(row=0, column=0, padx=5, sticky='nwe')
+        tk.Label(self.read_settings, text=self.part.filename).grid(row=0, column=1, padx=5, sticky='nwe')
+
+        tk.Label(self.read_settings, text="Number of Layers:").grid(row=1, column=0, padx=5, sticky='nwe')
+        tk.Label(self.read_settings, text=len(self.part.list_of_layers)).grid(row=1, column=1, padx=5, sticky='nwe')
+
+        tk.Label(self.read_settings, text="Ironing Enabled:").grid(row=2, column=0, padx=5, sticky='nwe')
+        tk.Label(self.read_settings, text=self.part.settings["ironing_enabled"]).grid(row=2, column=1, padx=5, sticky='nwe')
+
+        tk.Label(self.read_settings, text="Ironing Flow:").grid(row=3, column=0, padx=5, sticky='nwe')
+        tk.Label(self.read_settings, text=self.part.settings["ironing_flow"]).grid(row=3, column=1, padx=5, sticky='nwe')
+
+        tk.Label(self.read_settings, text="Ironing Inset:").grid(row=4, column=0, padx=5, sticky='nwe')
+        tk.Label(self.read_settings, text=self.part.settings["ironing_inset"]).grid(row=4, column=1, padx=5, sticky='nwe')
+
+        tk.Label(self.read_settings, text="Speed Ironing:").grid(row=5, column=0, padx=5, sticky='nwe')
+        tk.Label(self.read_settings, text=self.part.settings["speed_ironing"]).grid(row=5, column=1, padx=5, sticky='nwe')
+
+        # OPTIONS FOR IRONING
+        self.ironing_settings = ttk.LabelFrame(self.main_container, text="Apply Ironing")
+        self.ironing_settings.grid(row=2, column=0, padx=5, pady=5, sticky='nwe')
+
+        self.radio_var = tk.StringVar()
+        self.start_layer = tk.StringVar()
+        self.end_layer = tk.StringVar()
+        self.interval = tk.StringVar()
+        self.list_ironings = tk.StringVar()
+
+        self.radio_var.set("opt1")
+        self.list_ironings.set("[1,0,1,...,1,0,1]")
+
+        chk_btn_increment = tk.Radiobutton(self.ironing_settings, variable=self.radio_var, text="Use Intervals", value='opt1')
+        chk_btn_increment.grid(row=0, column=0, sticky="nw")
+
+        tk.Label(self.ironing_settings, text="Interval").grid(row=1, column=0)
+        tk.Spinbox(self.ironing_settings, textvariable=self.interval, from_=1, to=len(self.part.list_of_layers), wrap=True).grid(row=1, column=1, sticky="nw")
+
+        tk.Label(self.ironing_settings, text="Start at Layer").grid(row=2, column=0)
+        tk.Spinbox(self.ironing_settings, textvariable=self.start_layer, from_=1, to=len(self.part.list_of_layers), wrap=True).grid(row=2, column=1)
+
+        self.end_layer.set(str(len(self.part.list_of_layers)))
+        tk.Label(self.ironing_settings, text="End at Layer", width=8).grid(row=3, column=0)
+        tk.Spinbox(self.ironing_settings, textvariable=self.end_layer, from_=1, to=int(self.end_layer.get()), wrap=True).grid(row=3, column=1)
+
+        chk_btn_list = tk.Radiobutton(self.ironing_settings, variable=self.radio_var, text="Use a list", value='opt2')
+        chk_btn_list.grid(row=4, column=0, sticky="nw")
+
+        tk.Label(self.ironing_settings, text="Binary list of layers").grid(row=5, column=0)
+        tk.Entry(self.ironing_settings, textvariable=self.list_ironings).grid(row=5, column=1)
+
+        # OPTIONS FOR IRONING
+        self.advanced_settings = ttk.LabelFrame(self.main_container, text="Advanced Settings")
+        self.advanced_settings.grid(row=3, column=0, padx=5, pady=5, sticky='news')
+
+        self.flow_dir = tk.StringVar()
+        self.flow_dir.set("opta")
+
+        tk.Label(self.advanced_settings, text="Flow Direction").grid(row=0, column=0)
+        #chk_btn_flow_rep = tk.Radiobutton(self.advanced_settings, variable=self.flow_dir, text="Replicate", value='opta')
+        #chk_btn_flow_rep.grid(row=0, column=1, sticky="nw")
+        chk_btn_flow_par = tk.Radiobutton(self.advanced_settings, variable=self.flow_dir, text="Parallel", value='opta')
+        chk_btn_flow_par.grid(row=0, column=1, sticky="nw")
+        chk_btn_flow_perp = tk.Radiobutton(self.advanced_settings, variable=self.flow_dir, text="Perpendicular", value='optb')
+        chk_btn_flow_perp.grid(row=0, column=2, sticky="nw")
+
+        self.disable_flow = tk.IntVar()
+        self.disable_flow.set(0)
+        tk.Checkbutton(self.advanced_settings, text="Disable Flow", variable=self.disable_flow).grid(row=1, column=0, sticky="nw")
+
+        self.set_z_offset = tk.IntVar()
+        self.z_offset = tk.StringVar()
+        self.z_offset.set("0")
+        tk.Checkbutton(self.advanced_settings, text="Set Z offset [mm]", variable=self.set_z_offset).grid(row=2, column=0, sticky="nw")
+        tk.Entry(self.advanced_settings, textvariable=self.z_offset, width=12).grid(row=2, column=1, sticky="nw")
+
+        self.set_fan_speed = tk.IntVar()
+        self.fan_speed = tk.StringVar()
+        self.fan_speed.set("-1")
+        tk.Checkbutton(self.advanced_settings, text="Set Fan speed [%]", variable=self.set_fan_speed).grid(row=3, column=0, sticky="nw")
+        tk.Entry(self.advanced_settings, textvariable=self.fan_speed, width=12).grid(row=3, column=1, sticky="nw")
+
+        #tk.Checkbutton(self.advanced_settings, text="Flow direction").grid(row=4, column=0, sticky="nw")
+        #tk.Spinbox(self.advanced_settings, textvariable=self.interval, from_=1, to=self.ironing.number_of_layers, width=10).grid(row=4, column=2, sticky="ne")
+
+        # OPERATIONS
+        self.calculate_export = ttk.LabelFrame(self.main_container, text="Operations")
+        self.calculate_export.grid(row=4, column=0, padx=5, pady=15, sticky='nwe')
+        #ttk.Button(self.calculate_export, text="Calculate", command=self.calculate_ironing).grid(row=0, column=0, pady=5, padx=20)
+        #ttk.Button(self.calculate_export, text="Export", command=self.export_ironing).grid(row=0, column=1, pady=5, padx=20)
+        ttk.Button(self.calculate_export, text="Apply Ironing", command=self.export_ironing).grid(row=0, column=1, pady=5, padx=20)
+
+    def start_ironing_object(self):
+        root = add_new_file()
+        print("Open file %s" % root)
+        self.part = Part(root)
+        self.part.read_file()
+        self.part.get_settings_from_file()
+        self.part.get_part_instructions()
+
+        if self.part.check_for_ironing():
+            self.after_file_open()
+        else:
+            popup_message("Ironing is disabled")
+
+    def export_ironing(self):
+        root = save_new_file(self.part.ironing_file)
+        if root != '':
+
+            if self.radio_var.get() == 'opt1':
+                start_layer = int(self.start_layer.get()) - 1
+                end_layer = int(self.end_layer.get())
+                interval = int(self.interval.get())
+
+                list_for_ironing = [0] * len(self.part.list_of_layers)
+                for i in range(start_layer, end_layer, interval):
+                    list_for_ironing[i] = 1
+                print(list_for_ironing)
+
+            elif self.radio_var.get() == 'opt2':
+                separate = self.list_ironings.get().split(',')
+                if len(separate) != len(self.part.list_of_layers):
+                    popup_message("Number of layers mismatch")
+                else:
+                    list_for_ironing = [int(k) for k in separate]
+                    print(list_for_ironing)
+
+            fan_speed = 255 * float(self.fan_speed.get()) / 100
+            flow = bool(self.disable_flow.get())
+            z_offset = float(self.z_offset.get())
+            result = self.part.add_ironing_to_part(list_for_ironing, self.flow_dir.get(), flow, fan_speed, z_offset)
+
+            if result:
+                success_popup_message("Ironing caculated with success!")
+            else:
+                popup_message("Error")
+        else:
+            popup_message("Choose a file to export")
+
+
+if __name__ == '__main__':
+    app = GUI()
+    app.protocol("WM_DELETE_WINDOW", lambda: quit_confirmation(app))
+    app.mainloop()
